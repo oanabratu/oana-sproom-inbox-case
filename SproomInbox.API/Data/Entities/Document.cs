31160c4ef739925c005e5c1bffdf1fd4591e01cb
oanabratu <oanabratu2002@yahoo.com>
2022-03-08 16:25:23 +0200

    Added more comments

--- SproomInbox.API/Data/Entities/Document.cs
+++ SproomInbox.API/Data/Entities/Document.cs
@@ -1,20 +1,43 @@
 ﻿namespace SproomInbox.API.Data.Entities
 {
+    /// <summary>
+    /// Document entity that get stored in the database
+    /// </summary>
     public class Document
   {
-        //document id
+        /// <summary>
+        /// Document id - primary key
+        /// </summary>
         public Guid Id { get; set; }
-        //document type
+
+        /// <summary>
+        /// Document type 
+        /// </summary>
         public DocumentType DocumentType { get; set; }
-        //Current state
+
+        /// <summary>
+        /// Current state
+        /// </summary>
         public State State { get; set; }
-        //document state history
+
+        /// <summary>
+        /// Document state history
+        /// </summary>
         public ICollection<StateHistory> DocumentStates { get; set; }
-        //creation date of the document
+
+        /// <summary>
+        /// Creation date of the document
+        /// </summary>
         public DateTime CreationDate { get; set; }
-        //file reference
+
+        /// <summary>
+        /// File reference
+        /// </summary>
         public string FileReference { get; set; }
 
+        /// <summary>
+        /// Assigned To User
+        /// </summary>
         public string AssignedToUser { get; set; }
     }
 }
--- SproomInbox.API/Data/Entities/StateHistory.cs
+++ SproomInbox.API/Data/Entities/StateHistory.cs
@@ -5,9 +5,24 @@
     /// </summary>
     public class StateHistory
     {
+        /// <summary>
+        /// State Id - primary key
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Timestamp
+        /// </summary>
         public DateTime Timestamp { get; set; }
+
+        /// <summary>
+        /// State
+        /// </summary>
         public State State { get; set; } 
+
+        /// <summary>
+        /// Username that make the action
+        /// </summary>
         public string? Username { get; set; }
     }
 }
